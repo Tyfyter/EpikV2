c43e5e57b9adb63a7a45dbad99f19cec76a5df5f
Tyfyter <tyfyter2616@gmail.com>
2022-06-11 02:00:44 -0400

    Drax

--- Items/Mobile_Glitch_Present.cs
+++ Items/Mobile_Glitch_Present.cs
@@ -25,7 +25,7 @@ namespace EpikV2.Items {
             return true;
         }
         public override void RightClick(Player player) {
-            int random = Main.rand.Next(1, ItemLoader.ItemCount);
+            int random = Main.rand.NextBool(ItemLoader.ItemCount) ? ItemID.Drax : Main.rand.Next(1, ItemLoader.ItemCount);
 			Item item = new Item();
 			item.SetDefaults(random);
 			Item.NewItem(player.GetSource_OpenItem(item.type), player.Center, new Vector2(), random, Main.rand.Next(1, Math.Max(Math.Min(item.maxStack/Main.rand.Next(1,10), 500), 1)), false, 0, true);
