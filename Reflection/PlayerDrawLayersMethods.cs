d52e61e8b686aaae06af404f121d80a5d2bf611e
Tyfyter <tyfyter2616@gmail.com>
2026-08-03 14:15:50 -0600

    DrawSittingLegs never should have been private to begin with

--- Reflection/PlayerDrawLayersMethods.cs
+++ Reflection/PlayerDrawLayersMethods.cs
@@ -1,17 +1,11 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework.Graphics;
 using PegasusLib.Reflection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Terraria.DataStructures;
+using Terraria.ModLoader;
 
-namespace EpikV2.Reflection {
-	public class PlayerDrawLayersMethods : ReflectionLoader {
-		public delegate void Del_DrawSittingLegs(ref PlayerDrawSet drawinfo, Texture2D textureToDraw, Color matchingColor, int shaderIndex = 0, bool glowmask = false);
-		[ReflectionParentType(typeof(PlayerDrawLayers))]
-		public static Del_DrawSittingLegs DrawSittingLegs;
-	}
+namespace EpikV2.Reflection;
+public class PlayerDrawLayersMethods : ReflectionLoader {
+	public delegate void Del_DrawSittingLegs(ref PlayerDrawSet drawinfo, Texture2D textureToDraw, Color matchingColor, int shaderIndex = 0, bool glowmask = false, EquipType? equipType = null);
+	[ReflectionParentType(typeof(PlayerDrawLayers))]
+	public static Del_DrawSittingLegs DrawSittingLegs;
 }
